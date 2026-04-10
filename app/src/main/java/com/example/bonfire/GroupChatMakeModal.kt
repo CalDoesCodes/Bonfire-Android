a24b645c7656248445635462a81b952be32daf0c
Amy Gonzalez <amaice8@gmail.com>
2026-04-10 16:09:46 -0500

    debugging group chat creation modal

--- app/src/main/java/com/example/bonfire/GroupChatMakeModal.kt
+++ app/src/main/java/com/example/bonfire/GroupChatMakeModal.kt
@@ -1,28 +1,20 @@
 package com.example.bonfire
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
-import android.media.MediaPlayer
-import android.media.MediaRecorder
-import android.net.Uri
-import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContextCompat
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
 import com.google.firebase.storage.storage
-import java.util.UUID
-import java.io.File
 
 /**
  * Modal popup for creating a group chat.
@@ -36,23 +28,27 @@ internal class GroupChatMakeModal {
     val TAG = "Group chat make modal"
     lateinit var context: Context
     lateinit var linearLayout : LinearLayout
+    val uid = FirebaseAuth.getInstance().currentUser?.uid
 
 
     fun openModal(context: Context, friendIDs: List<String>) {
         val activity = context as Activity
         dialogView = activity.layoutInflater.inflate(R.layout.groupchat_make_modal, null)
         this.context = context
-        this.linearLayout = context.findViewById(R.id.groupchat_make_modal_linearLayout)
+        this.linearLayout = dialogView.findViewById(R.id.groupchat_make_modal_linearLayout)
 
         val builder = AlertDialog.Builder(context)
         val dialog = builder.create()
         dialog.setView(dialogView)
         dialog.show()
 
+        val editText = dialogView.findViewById<TextInputEditText>(R.id.groupChat_edit)
         createBtn = dialogView.findViewById(R.id.createBtn)
         createBtn.setOnClickListener {
-            if(createBtn.isEnabled) {
-                createGroupChat()
+            // Only valid if group chat is named and has 2+ friends
+            if(addedFriendIDs.size >= 2 &&
+                editText.text.toString().isNotEmpty()) {
+                createGroupChat(editText.text.toString())
                 dialog.dismiss()
             }
         }
@@ -68,7 +64,6 @@ internal class GroupChatMakeModal {
     }
 
     private fun generateListOfFriends(friendIDs: List<String>) {
-        val friendView = LayoutInflater.from(context).inflate(R.layout.friend_add_groupchat_layout, linearLayout, false)
 
         for (friendID in friendIDs){
             // Get avatar of friend
@@ -76,10 +71,11 @@ internal class GroupChatMakeModal {
             docRef.get()
             .addOnSuccessListener { document ->
                 if (document != null && document.exists()) {
+                    val friendView = LayoutInflater.from(context).inflate(R.layout.friend_add_groupchat_layout, linearLayout, true)
                     val friendData = document.data ?: return@addOnSuccessListener
                     // Friend data found, update avatar
                     helper.setProfilePicture(context, (friendData["avatar"] ?: "") as String, friendView.findViewById<ImageView>(R.id.avatar))
-                    linearLayout.addView(friendView)
+
                 } else {
                     Log.d(TAG, "No such document")
                 }
@@ -116,7 +112,27 @@ internal class GroupChatMakeModal {
         createBtn.text = "Create group chat"
     }
 
-    private fun createGroupChat() {
-        TODO("Not yet implemented")
+    private fun createGroupChat(chatName: String) {
+        val storage = Firebase.storage
+        try {
+            // Get URI of default profile picture
+            val avatarPath = helper.firebasePath + "/Profile_Pictures/logo.png"
+            val groupChatPath = "groupChats"
+            val gsReference = storage.getReferenceFromUrl(avatarPath)
+            gsReference.downloadUrl.addOnSuccessListener { uri ->
+                val messageData = hashMapOf(
+                    "avatar" to uri,
+                    "createdAt" to Timestamp.now(),
+                    "createdBy" to uid,
+                    "memberIds" to addedFriendIDs,
+                    "name" to chatName,
+                    "type" to "group"
+                )
+
+                db.collection(groupChatPath).document().set(messageData)
+            }
+        } catch (e : IllegalArgumentException){
+            Log.e(TAG, "Icon not found: $e")
+        }
     }
 }
